648d35c4a776c3be9785a7b7272a4ee763c9417d
Pharap <pharap@hotmail.com>
2017-05-25 14:39:26 +0100

    Made the '^' character call WaitForButton

---
+++ game.ino
@@ -0,0 +1,258 @@
+// Main game loop, use interactive false for the level intro
+int doGame(bool interactive)
+{
+  byte exitCode = !interactive ? STARTLEVELINTRO : ( playerx > arduboy.width() - 3 ? WIN : PLAYING);
+  arduboy.clear();
+
+  // Streets
+  const int laneoffset = 61;
+  arduboy.drawLine(20, 0, 20, arduboy.height()); // Lane 1
+  arduboy.drawLine(20 + laneoffset, 0, 20 + laneoffset, arduboy.height()); // Lane 2
+
+  const byte streetLine = 16;
+  for (byte i = 0; i < arduboy.height(); i += streetLine)
+  {
+    arduboy.drawLine(39, i, 39, i + 7);  // Lane 1
+    arduboy.drawLine(39 + laneoffset, i, 39 + laneoffset, i + 7); // Lane 2
+  }
+
+  arduboy.drawLine(58, 0, 58, arduboy.height()); // Lane 1
+  arduboy.drawLine(58 + laneoffset, 0, 58 + laneoffset, arduboy.height()); // Lane 2
+
+  // Player
+  playerSpecs player = playerTypes[currentLevel];
+
+  if (playerx == INVALID)
+    playerx = -player.width;
+
+  if (playery == INVALID)
+    playery = (arduboy.height() - player.height) / 2;
+
+  arduboy.drawBitmap(floor(playerx), floor(playery), player.mask, player.width, player.height, BLACK);
+  arduboy.drawBitmap(floor(playerx), floor(playery), player.bitmap, player.width, player.height);
+
+  // TODO: Make this dynamic
+  const byte blockx = 34, blocky = 30, blockw = 11, blockh = 5;
+
+  if (currentLevel < 2)
+  {
+    // Road blocks
+    arduboy.drawBitmap(blockx, blocky, block, blockw, blockh); // Lane 1
+  }
+
+  if (interactive)
+  {
+    if (arduboy.pressed(B_BUTTON))
+    {
+      // Pause game
+      doPause();
+      return 1;
+    }
+
+    int multiplier = 1;
+    if ( millis() > debounce)
+    {
+      if (arduboy.pressed(A_BUTTON))
+      {
+        debounce = millis() + 500;
+        multiplier = FORWARD_BOOST;
+      }
+    }
+    else if (!arduboy.pressed(A_BUTTON))
+      debounce -= 50; // Decrease the wait
+    else
+      debounce += 50; // Increase the wait
+
+    double newx = playerx, newy = playery;
+    if (arduboy.pressed(LEFT_BUTTON))    newx -= multiplier * player.vx;
+    if (arduboy.pressed(RIGHT_BUTTON))   newx += multiplier * player.vx;
+    if (arduboy.pressed(UP_BUTTON))      newy -= multiplier * player.vy;
+    if (arduboy.pressed(DOWN_BUTTON))    newy += multiplier * player.vy;
+
+    // Check if the new position is valid
+    bool valid = false;
+
+    if (newx >= 0 && newy >= -2 && (newy + player.height) <= arduboy.height() + 2)
+    {
+      if (currentLevel < 2)
+        valid = !arduboy.collide({(byte)newx, (byte)newy, player.width, player.height},
+      {blockx + 2, blocky + 2, blockw - 4, blockh - 4});
+      else
+        valid = true;
+    }
+
+    if (valid && (playerx != newx || playery != newy)) // Update
+    {
+      if (multiplier > 1)
+        tunes.tone(1100, 30);
+      else if (wait < millis())
+      {
+        tunes.tone(600, 6);
+        wait = millis() + 200;
+      }
+      playerx = newx;
+      playery = newy;
+    }
+  }
+
+  // Border
+  arduboy.drawBitmap(0, 0, fence, 4, 32);
+  arduboy.drawBitmap(0, 32, fence, 4, 32);
+  arduboy.drawBitmap(arduboy.width() - 5, 0, right_foliage, 6, 32);
+  arduboy.drawBitmap(arduboy.width() - 5, 32, right_foliage, 6, 32);
+
+  // Enemies
+  byte enemy = 0;
+  for (byte e = enemiesOffset[currentLevel]; e < enemiesOffset[currentLevel + 1]; e++)
+  {
+    if (currentFrame > enemies[e].wait)
+    {
+      enemySpecs specs = enemyTypes[enemies[e].type];
+      enemiesPos[enemy] += enemies[e].vy;
+
+      if (playerx + player.width >= enemies[e].x && playerx <= enemies[e].x + specs.width)
+        arduboy.drawBitmap(enemies[e].x, floor(enemiesPos[enemy]), specialCar == e ? car2mask : specs.mask , specs.width, specs.height, BLACK);
+      arduboy.drawBitmap(enemies[e].x, floor(enemiesPos[enemy]), specialCar == e ? car2 : specs.bitmap, specs.width, specs.height);
+
+      // Loop
+      if (enemiesPos[enemy] < -32)
+      {
+        enemiesPos[enemy] = arduboy.height();
+
+        if (specialCar == e)
+          specialCar = -1;
+        else if (specialCar == -1 && enemies[e].type == CAR)
+          if (random(0, 50) == 7)
+          {
+            if (specialCarBonus)
+            {
+              score += 500;
+              specialCarBonus = false;
+            }
+            tunes.stopScore();
+            tunes.playScore(car2music);
+            specialCar = e;
+          }
+      }
+
+      // Check collision
+      if (arduboy.collide({(byte)playerx + 2, (byte)playery + 2, (byte)(player.width - 4), (byte)(player.height - 4)},
+      {enemies[e].x + 2, (byte)(enemiesPos[enemy] + 2), (byte)(specs.width - 4), (byte)(specs.height - 4)}))
+      {
+        doCrash();
+        exitCode = LOSE;
+      }
+    }
+    enemy++;
+  }
+
+  // Foliage
+  arduboy.drawBitmap(63, -10, tree, 18, 18);
+  arduboy.drawBitmap(57, 14, tree, 18, 18);
+  arduboy.drawBitmap(65, 37, tree, 18, 18);
+  arduboy.drawBitmap(56, 58, tree, 18, 18);
+
+  currentFrame++;
+
+#ifdef MIRRORING
+  Serial.write(arduboy.getBuffer(), arduboy.width() * arduboy.height() / 8);
+#endif
+
+  arduboy.display();
+  return exitCode;
+}
+
+// Shows and handles the main menu
+void doMainMenu()
+{
+  bool exit = false;
+  char option = 0;
+
+  // Track menu movement pattern
+  byte pos = 0;
+  const byte max = 17;
+  byte seq[] = {2, 1, 2, 2, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 2};
+  byte mov[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  do {
+    doIntroTheme();
+    arduboy.clear();
+    arduboy.drawCompressed(14, 2, intro);
+    arduboy.setCursor(24, 55);
+    arduboy.print((char)0x11);
+
+    switch (option)
+    {
+      case 0: arduboy.setCursor(33, 55); arduboy.print("Start game"); break;
+      case 1: arduboy.setCursor(33, 55); arduboy.print("Sound: "); arduboy.print(arduboy.audio.enabled() ? " ON" : "OFF"); break;
+      case 2: arduboy.setCursor(39, 55); arduboy.print("Hi-score"); break;
+      case 3: arduboy.setCursor(43, 55); arduboy.print("Credits"); break;
+      case 4: arduboy.setCursor(52, 55); arduboy.print("Help"); break;
+    }
+    arduboy.setCursor(arduboy.width() - 24 - 8, 55);
+    arduboy.print((char)0x10);
+
+#ifdef MIRRORING
+    Serial.write(arduboy.getBuffer(), arduboy.width() * arduboy.height() / 8);
+#endif
+
+    arduboy.display();
+    arduboy.pollButtons();
+
+    if (arduboy.justPressed(B_BUTTON))
+    {
+      while (arduboy.pressed(B_BUTTON));
+
+      switch (option)
+      {
+        case 0: {
+            exit = true;
+            bool ee = true;
+            for (byte i = 0; i < max; i++)
+              if (mov[pos++] != seq[i])
+              {
+                ee = false;
+                break;
+              }
+              else
+                pos %= max;
+
+            if (ee)
+            {
+              FORWARD_BOOST += 500;
+              doWinSoundEffect();
+            }
+          } break;
+        case 1: tunes.stopScore(); arduboy.audio.toggle(); arduboy.audio.saveOnOff(); break;
+        case 2: animateDialog(monkey2, (score == 0 && highscore == 0) ? hiscore0 : hiscore1); waitForButton(); break;
+        case 3: animateDialog(qr, credits0); waitForButton(); animateDialog(qr, credits01); waitForButton(); animateDialog(qr, credits1); animateDialog(qr, credits2); waitForButton(); break;
+        case 4: animateDialog(monkey2, tip0); doRandomTip(); waitForButton(); break;
+      }
+    }
+    else
+    {
+      if (arduboy.justPressed(LEFT_BUTTON))
+      {
+        mov[pos++] = 1;
+        option--;
+        tunes.tone(1000, 30);
+      }
+
+      if (arduboy.justPressed(RIGHT_BUTTON))
+      {
+        mov[pos++] = 2;
+        option++;
+        tunes.tone(1000, 30);
+      }
+
+      pos %= max; // Bounded
+      option = option < 0 ? 4 : (option > 4 ? 0 : option);
+    }
+  } while (!exit);
+
+  tunes.stopScore();
+  tunes.tone(987, 120);
+  arduboy.delayShort(60);
+  tunes.tone(1318, 400);
+  gameState = INTRO;
+}
---
+++ loop.ino
@@ -0,0 +1,133 @@
+void loop()
+{
+  if (!arduboy.nextFrame())
+    return;
+
+  switch (gameState)
+  {
+    case SPLASH:
+      doIntroTheme();
+      doSplash();
+
+    case MAINMENU:
+      doIntroTheme();
+      doMainMenu();
+      break;
+
+    case INTRO:
+      doIntro();
+
+    case SKIPINTRO:
+      specialCarBonus = false;
+      score = 0;
+      currentLevel = 0;
+      lives = STARTLIVES;
+      gameState = STARTLEVEL;
+      break;
+
+    case END:
+      if (!tunes.playing())
+        tunes.playScore(gameovertheme);
+
+      arduboy.clear();
+      if (wait < millis())
+      {
+        // Show gameover text
+        arduboy.setCursor(12, 25);
+        arduboy.setTextSize(2);
+        arduboy.print("GAME OVER");
+        arduboy.setTextSize(1);
+      }
+
+      if (millis() > debounce)
+      {
+        // Show score
+        arduboy.setCursor(0, arduboy.height() - 7);
+        arduboy.print("Score: ");
+        arduboy.print(score);
+
+        arduboy.setCursor(0, 0);
+        arduboy.print("Hi: ");
+        arduboy.print(highscore);
+
+        while (!arduboy.notPressed(A_BUTTON+B_BUTTON)); // Wait for button release
+
+        arduboy.setCursor(arduboy.width() - 40, 0);
+        arduboy.print("Retry");
+        arduboy.setCursor(arduboy.width() - 34, arduboy.height() - 7);
+        arduboy.print("Menu");
+        arduboy.display();
+
+        do
+        {
+          if ( waitForButton(false))
+            gameState = MAINMENU;
+          else if ( waitForButton(false, true, arduboy.height()))
+            gameState = SKIPINTRO;
+        } while (gameState == END);
+
+        while (!arduboy.notPressed(A_BUTTON+B_BUTTON)); // Wait for button release
+        tunes.stopScore();
+      }
+      arduboy.display();
+
+      break;
+
+    case WIN:
+      doWinDialog();
+      gameState = STARTLEVEL;
+      break;
+
+    case GAMEOVER:
+    case LOSE:
+      if (--lives <= 0)
+      {
+        animateDialog(monkeybig, gameover2);
+        arduboy.delayShort(1000);
+        gameState = END;
+        wait =  millis() + (arduboy.audio.enabled() ? 2000 : 0);
+        debounce = wait + 2000;
+      }
+      else
+      {
+        animateDialog(monkeybig, gameover1, true);
+        waitForButton();
+        gameState = STARTLEVEL;
+      }
+      break;
+
+    case STARTLEVEL:
+      for (byte i = 0; i < enemiesMax; i++)
+        enemiesPos[i] = arduboy.height();
+
+      tunes.playScore(levelintro);
+      playerx = INVALID;
+      playery = INVALID;
+      currentFrame = 0;
+      specialCar = -1;
+
+    case STARTLEVELINTRO:
+      if (playerx != INVALID)
+        playerx += 0.15;
+
+      if (playerx > 3)
+      {
+        gameState = PLAYING;
+        currentLevelTime = millis();
+      }
+
+    default: // Level
+      if (!tunes.playing() && gameState == PLAYING)
+        tunes.playScore(level);
+
+      gameState = doGame(gameState == PLAYING);
+
+#ifdef DEBUG
+      Serial.print("PLAYING, NEXT STATE: ");
+      Serial.println(gameState);
+#endif
+      break;
+  }
+}
+
+
