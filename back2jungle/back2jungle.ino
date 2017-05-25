2cffb17ec1d37695fd6938630d82f648696d2643
Pharap <pharap@hotmail.com>
2017-05-25 13:39:42 +0100

    Changed score and highscore to unsigned int
    Saves 78 bytes.

--- back2jungle/back2jungle.ino
+++ back2jungle/back2jungle.ino
@@ -30,7 +30,7 @@ char lives = STARTLIVES;
 unsigned long currentFrame = 0, currentLevelTime = 0;
 byte currentLevel = STARTINGLEVEL, gameState = SPLASH;
 unsigned long wait = 0, debounce = 0;
-int score = 0, highscore = 0;
+unsigned int score = 0, highscore = 0;
 unsigned char specialCar;
 bool specialCarBonus = true, fasterDialogs = false;
 double playerx, playery;
